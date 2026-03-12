70dca0047145acacad9dc04e2c1a4d01c65d31f4
Andriy Butnar <xpaand4@gmail.com>
2026-03-12 22:49:50 +0200

    No base path in build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.PNG', '**/*.MP4'],
-  base: 'https://drummer01.github.io/invite/',
 })

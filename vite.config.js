12f1d5930401fa2a231a75c0fc1709a8174bb9f9
Andriy Butnar <xpaand4@gmail.com>
2026-03-12 22:37:57 +0200

    Adjust final version icons, styles etc

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.PNG', '**/*.MP4'],
-  base: '/invite/',
+  base: 'https://drummer01.github.io/invite/',
 })
